a522cdf35b4051476a0a1650e27884af49416fce
wiladhianty <wiladhianty@gmail.com>
2021-06-30 15:38:35 +0700

    tambah tugas

---
+++ tugas27.js
@@ -0,0 +1,14 @@
+var var11 = "No"
+var var12 = " rumah saya "
+var var13 = 30
+var var14 = "A"
+
+var var21 = "Jalan pangeran Tirtayasa No "
+var var22 = 70
+
+
+var hasil1 = var11 + var12 + String(var13) + var14
+var hasil2 = var21 + String(var22)
+
+console.log(hasil1)
+console.log(hasil2)
